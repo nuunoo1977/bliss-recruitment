51abb5876bb572358601e2702fcdd656a097da45
Nuno Felicio <nuunoo@gmail.com>
2018-08-07 23:39:20 +0100

    Added 1st version of main components and routing

--- bliss-recruitment/src/app/app.module.ts
+++ bliss-recruitment/src/app/app.module.ts
@@ -3,17 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { LoadingScreenComponent } from './components/loading-screen/loading-screen.component';
+import { QuestionsComponent } from './components/questions/questions.component';
+import { QuestionDetailComponent } from './components/question-detail/question-detail.component';
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoadingScreenComponent,
+        QuestionsComponent,
+        QuestionDetailComponent
     ],
     imports: [
         BrowserModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
---
+++ bliss-recruitment/src/app/components/question-detail/question-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-question-detail',
+    templateUrl: './question-detail.component.html',
+    styleUrls: ['./question-detail.component.scss']
+})
+export class QuestionDetailComponent {
+    constructor() {
+    }
+}
