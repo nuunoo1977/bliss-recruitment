4af1fa180f2c11fe266bde44d29c5f07aa9473ba
Nuno Felicio <nuunoo@gmail.com>
2018-08-08 00:51:00 +0100

    Login screen implemented

--- bliss-recruitment/src/app/components/loading-screen/loading-screen.component.ts
+++ bliss-recruitment/src/app/components/loading-screen/loading-screen.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-loading-screen',
     templateUrl: './loading-screen.component.html',
     styleUrls: ['./loading-screen.component.scss']
 })
-export class LoadingScreenComponent {
-    constructor() {
+export class LoadingScreenComponent implements AfterViewInit {
+
+    showSpinner = true;
+
+    constructor(
+        private http: HttpClient,
+        private router: Router
+    ) {
     }
+
+    ngAfterViewInit() {
+        this.checkServerHealth();
+    }
+
+    checkServerHealth() {
+        this.showSpinner = true;
+        this.http.get('https://private-bbbe9-blissrecruitmentapi.apiary-mock.com/health')
+            .subscribe((result: any) => {
+                if(result && result.status == 'OK') {
+                    this.router.navigate(['/questions']);
+                }
+                else {
+                    this.showSpinner = false;    
+                }
+            }, () => {
+                this.showSpinner = false;
+            });
+    }
+
 }
+
