be77ea9d6b2f44321ca74daa3aa5680ad20cb615
Nuno Felicio <nuunoo@gmail.com>
2018-08-08 22:11:39 +0100

    Added models

---
+++ bliss-recruitment/src/app/shared/question.ts
@@ -0,0 +1,10 @@
+import { QuestionChoice } from "./question-choice";
+
+export class Question {
+    id: number;
+    question: string;
+    image_url: string;
+    thumb_url: string;
+    published_at: Date;
+    choices: QuestionChoice[]
+};
